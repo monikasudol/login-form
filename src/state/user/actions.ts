d58b9426cfea574da7e423b635c0e7da2426065d
Monika Sudol <monikaasudol@gmail.com>
2020-04-30 18:21:48 +0200

    Removed unused actions and small styles changes

--- src/state/user/actions.ts
+++ src/state/user/actions.ts
@@ -3,7 +3,6 @@ import { AnyAction } from 'redux';
 const LOGIN = 'LOGIN';
 const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 const LOGIN_FAILURE = 'LOGIN_FAILURE';
-const RESET_LOGIN_STATUS = 'RESET_LOGIN_STATUS';
 
 const loginAction = ({ email, password }: { email?: string, password?: string }) => ({
   type: LOGIN,
@@ -52,7 +51,6 @@ export default {
   LOGIN,
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
-  RESET_LOGIN_STATUS,
   loginAction,
   loginSuccessAction,
   loginFailureAction,
